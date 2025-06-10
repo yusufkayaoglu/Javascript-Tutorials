e64de2f6739d0c8d6d26cf5f57f4bc653388bc9f
Yusuf Kayaoglu <yusuf.kayaoglu@gazi.edu.tr>
2025-06-10 13:04:40 +0300

    Yeni özellik eklendi

---
+++ Arrow Functions/app.js
@@ -0,0 +1,40 @@
+// function yazdir() {
+//   console.log("Merhaba");
+// }
+
+// yazdir();
+
+//Arrow Function
+// const yazdir = () => {
+//   console.log("Merhaba");
+// };
+
+// yazdir();
+
+// const yazdir = (firstName, lastName) => {
+//   console.log("Merhaba", firstName, lastName);
+// };
+
+//Eğer tek bir satırla işlem yapılacaksa kıvırcık parantezler de silinebilir!!!
+// const yazdir = (firstName, lastName) =>
+//   console.log("Merhaba", firstName, lastName);
+
+// const yazdir = (firstName, lastName) => {
+//   console.log("Merhaba", firstName, lastName);
+//   let a = 5;
+//   console.log(a);
+//   console.log("Yusuf");
+// };
+
+// yazdir("Yusuf", "Kayaoğlu");
+
+//Tek parametre alıyorsa parantezleri kaldırabilirsin!!
+// const yazdir = (firstName) => {
+//   console.log("Merhaba", firstName);
+// };
+
+// yazdir("Enes");
+
+// const kupAl = (x) => x * x * x;
+
+// console.log("Değer : ", kupAl(3));
